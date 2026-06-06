1a098f794517a3d5d23f610c06e08c7ba5ae88a9
tuhinbby92-web <tuhinbby92@gmail.com>
2026-06-06 13:02:07 +0600

    Update admin.js

--- Script/commands/admin.js
+++ Script/commands/admin.js
@@ -19,15 +19,15 @@ module.exports.run = async function({ api, event }) {
     var callback = () => api.sendMessage({
         body: `
 ┏━━━━━━━━━━━━━━━━━━━━━┓
-┃✡   🌟 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 🌟      
+┃   🌟 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 🌟      
 ┣━━━━━━━━━━━━━━━━━━━━━┫
-┃ 👤 𝐍𝐚𝐦𝐞      : 𝐓𝐔𝐇𝐈𝐍 ッ
+┃ 👤 𝐍𝐚𝐦𝐞      : TUHIN BBY ッ
 ┃ 🚹 𝐆𝐞𝐧𝐝𝐞𝐫    : 𝐌𝐚𝐥𝐞
 ┃ ❤️ 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧  : সিঙ্গেল 🥲 
-┃ 🎂 𝐀𝐠𝐞       : 17+
+┃ 🎂 𝐀𝐠𝐞       : 18+
 ┃ 🕌 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧  : 𝐈𝐬𝐥𝐚𝐦
 ┃ 🏫 𝐄𝐝𝐮𝐜𝐚𝐭𝐢𝐨𝐧 : 𝐒𝐞𝐜𝐨𝐧𝐝𝐚𝐫𝐲 𝐒𝐜𝐡𝐨𝐨𝐥 
-┃ 🏡 𝐀𝐝𝐝𝐫𝐞𝐬𝐬  : 𝐓𝐡𝐚𝐤𝐮𝐫𝐠𝐚𝐨𝐧, 𝐁𝐚𝐧𝐠𝐥𝐚𝐝𝐞𝐬𝐡  
+┃ 🏡 𝐀𝐝𝐝𝐫𝐞𝐬𝐬  : Thakurgaon, Bangladesh  
 ┣━━━━━━━━━━━━━━━━━━━━━┫
 ┃ 🎭 𝐓𝐢𝐤𝐭𝐨𝐤  : uffbby92 
 ┃ 📢 Gf: দেখতে খারাপ সেজন্য গার্লফ্রেন্ড নাই 
@@ -39,7 +39,7 @@ module.exports.run = async function({ api, event }) {
         attachment: fs.createReadStream(__dirname + "/cache/1.png")
     }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"));
   
-    const imageURL = "https://imgur.com/a/2tFPDLf";
+    const imageURL = "https://i.imgur.com/AM0I5nZ.jpeg";
 
     return request(encodeURI(imageURL))
         .pipe(fs.createWriteStream(__dirname + '/cache/1.png'))
